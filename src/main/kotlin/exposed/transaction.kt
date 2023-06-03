4986d6f72b891b4c8e64d5c117534be74a361eb7
atalik <artur.talik@apzumi.pl>
2023-06-03 13:48:02 +0200

    deleted unnecessary annotation

--- src/main/kotlin/exposed/transaction.kt
+++ src/main/kotlin/exposed/transaction.kt
@@ -4,10 +4,8 @@ import arrow.core.Either
 import arrow.core.raise.Raise
 import arrow.core.raise.fold
 import org.jetbrains.exposed.sql.transactions.transaction
-import kotlin.experimental.ExperimentalTypeInference
 
-@OptIn(ExperimentalTypeInference::class)
-fun <Error, A> transactionButVeryChad(@BuilderInference block: Raise<Error>.() -> A): Either<Error, A> =
+fun <Error, A> transactionButVeryChad(block: Raise<Error>.() -> A): Either<Error, A> =
     fold(
         { transaction { block.invoke(this@fold) } },
         { Either.Left(it) },
